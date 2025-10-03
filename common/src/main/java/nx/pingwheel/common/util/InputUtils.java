6d154082fd8fdf720985fb9b35818e4b47f47741
Luca v. d. W. <github@luken.cc>
2025-10-04 00:25:45 +0200

    :recycle: use language utils for key binding identifiers

--- common/src/main/java/nx/pingwheel/common/util/InputUtils.java
+++ common/src/main/java/nx/pingwheel/common/util/InputUtils.java
@@ -3,6 +3,7 @@ package nx.pingwheel.common.util;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.world.phys.HitResult;
+import nx.pingwheel.common.resource.LanguageUtils;
 import org.lwjgl.glfw.GLFW;
 
 import static nx.pingwheel.common.CommonClient.Game;
@@ -10,10 +11,10 @@ import static nx.pingwheel.common.CommonClient.Game;
 public class InputUtils {
 	InputUtils() {}
 
-	private static final String SETTINGS_CATEGORY = "key.category.pingwheel.name";
-	public static final KeyMapping KEY_BINDING_PING = new KeyMapping("key.pingwheel.ping_location", InputConstants.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_5, SETTINGS_CATEGORY);
-	public static final KeyMapping KEY_BINDING_SETTINGS = new KeyMapping("key.pingwheel.open_settings", InputConstants.Type.KEYSYM, -1, SETTINGS_CATEGORY);
-	public static final KeyMapping KEY_BINDING_NAME_LABELS = new KeyMapping("key.pingwheel.name_labels", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, SETTINGS_CATEGORY);
+	private static final String SETTINGS_CATEGORY = LanguageUtils.keyOf("key.category", "name");
+	public static final KeyMapping KEY_BINDING_PING = new KeyMapping(LanguageUtils.keyOf("key", "ping_location"), InputConstants.Type.MOUSE, GLFW.GLFW_MOUSE_BUTTON_5, SETTINGS_CATEGORY);
+	public static final KeyMapping KEY_BINDING_SETTINGS = new KeyMapping(LanguageUtils.keyOf("key", "open_settings"), InputConstants.Type.KEYSYM, -1, SETTINGS_CATEGORY);
+	public static final KeyMapping KEY_BINDING_NAME_LABELS = new KeyMapping(LanguageUtils.keyOf("key", "name_labels"), InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, SETTINGS_CATEGORY);
 
 	public static boolean consumePingHotkey() {
 		if (!KEY_BINDING_PING.same(Game.options.keyPickItem)) {
