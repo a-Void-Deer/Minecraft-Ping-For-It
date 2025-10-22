1a97504ce3ea59668be9bde3aa6c47216087ac0c
Luca v. d. W. <github@luken.cc>
2025-10-22 21:38:08 +0200

    :alien: migrate v1.12.0 to 1.21.3

--- common/src/main/java/nx/pingwheel/common/compat/LegacyMigrationHandler.java
+++ common/src/main/java/nx/pingwheel/common/compat/LegacyMigrationHandler.java
@@ -87,7 +87,7 @@ public class LegacyMigrationHandler {
 			msg.append(" to ");
 			msg.append(Component.literal("\"assets/pingwheel\"").withStyle(ChatFormatting.GRAY));
 
-			Game.player.sendSystemMessage(LanguageUtils.withModPrefix(msg));
+			Game.player.displayClientMessage(LanguageUtils.withModPrefix(msg), false);
 			notifyDeprecatedResourcePack = false;
 		}
 	}
