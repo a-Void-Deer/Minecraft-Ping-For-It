ede6d4c512946b1e80e4902ef9c35797bef35c6e
Luca v. d. W. <github@luken.cc>
2025-10-22 21:39:10 +0200

    :alien: migrate v1.12.0 to 1.21.4

--- forge/src/main/java/nx/pingwheel/forge/ForgeMain.java
+++ forge/src/main/java/nx/pingwheel/forge/ForgeMain.java
@@ -8,6 +8,7 @@ import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.EventNetworkChannel;
 import nx.pingwheel.common.CommonServer;
@@ -16,6 +17,7 @@ import nx.pingwheel.common.network.PingLocationC2SPacket;
 import nx.pingwheel.common.network.PingLocationS2CPacket;
 import nx.pingwheel.common.network.UpdateChannelC2SPacket;
 import nx.pingwheel.common.resource.LanguageUtils;
+import nx.pingwheel.forge.platform.PlatformContextServiceImpl;
 import nx.pingwheel.forge.platform.PlatformNetworkServiceImpl;
 import org.apache.logging.log4j.util.TriConsumer;
 
@@ -32,10 +34,12 @@ public class ForgeMain {
 	public static final EventNetworkChannel UPDATE_CHANNEL_C2S = ChannelBuilder.named(UpdateChannelC2SPacket.PACKET_ID).optional().eventNetworkChannel();
 
 	@SuppressWarnings({"java:S1118", "the public constructor is required by forge"})
-	public ForgeMain() {
+	public ForgeMain(FMLJavaModLoadingContext context) {
 		CommonServer.INSTANCE.onInit();
 
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ForgeClient::new);
+		PlatformContextServiceImpl.context = context;
+
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> new ForgeClient(context));
 
 		PlatformNetworkServiceImpl.CHANNEL_MAP.put(PingLocationC2SPacket.PACKET_ID, PING_LOCATION_CHANNEL_C2S);
 		PlatformNetworkServiceImpl.CHANNEL_MAP.put(PingLocationS2CPacket.PACKET_ID, PING_LOCATION_CHANNEL_S2C);
