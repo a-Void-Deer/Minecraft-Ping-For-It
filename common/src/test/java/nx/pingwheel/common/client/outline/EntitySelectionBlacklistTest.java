b5a43b9a0633eb1e24e4b2677eb0c4919f867b48
Void-Deer <void_deer@163.com>
2026-08-26 22:47:15 +0800

    compat:ignore create super glue by default

--- common/src/test/java/nx/pingwheel/common/client/outline/EntitySelectionBlacklistTest.java
+++ common/src/test/java/nx/pingwheel/common/client/outline/EntitySelectionBlacklistTest.java
@@ -1,7 +1,11 @@
 package nx.pingwheel.common.client.outline;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import net.minecraft.world.entity.Entity;
 
 import nx.pingwheel.common.math.EntitySelectionBlacklist;
 
@@ -51,5 +55,44 @@ class EntitySelectionBlacklistTest {
 		assertTrue(blacklist.isBlacklisted(second));
 
 		secondRegistration.close();
+	}
+
+	@Test
+	void optionalCreateRegistrationIsOnlyExercisedWhenItsBoundaryIsAvailable() throws Exception {
+		ClassLoader loader = EntitySelectionBlacklist.class.getClassLoader();
+		final Class<?> adapterClass;
+		final Class<?> superGlueClass;
+		try {
+			adapterClass = Class.forName(
+				"nx.pingwheel.neoforge.integration.create.CreateEntityOutlineAdapter", false, loader);
+			superGlueClass = Class.forName(
+				"com.simibubi.create.content.contraptions.glue.SuperGlueEntity", false, loader);
+		} catch (ClassNotFoundException | LinkageError absentOptionalIntegration) {
+			return;
+		}
+
+		Object uninitializedSuperGlue = allocateWithoutConstructor(superGlueClass);
+		if (!(uninitializedSuperGlue instanceof Entity superGlue)) {
+			return;
+		}
+
+		var register = adapterClass.getMethod("register");
+		var close = adapterClass.getMethod("close");
+		try {
+			register.invoke(null);
+			assertTrue(EntitySelectionBlacklist.INSTANCE.isBlacklisted(superGlue));
+		} finally {
+			close.invoke(null);
+		}
+
+		assertFalse(EntitySelectionBlacklist.INSTANCE.isBlacklisted(superGlue));
+	}
+
+	private static Object allocateWithoutConstructor(Class<?> type) throws Exception {
+		Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+		Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+		unsafeField.setAccessible(true);
+		Object unsafe = unsafeField.get(null);
+		return unsafeClass.getMethod("allocateInstance", Class.class).invoke(unsafe, type);
 	}
 }
